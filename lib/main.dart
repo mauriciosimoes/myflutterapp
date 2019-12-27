9228a13586da5e1cd431aac04ced882e85526bb6
mauriciosimoes <43817204+mauriciosimoes@users.noreply.github.com>
2019-12-27 10:37:31 -0300

    Testando conflito no

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  // Testing conflit on Version control.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
